b7a9340e53f5e1693ef717f55f4cf1c4689e39f3
Ben Orenstein <ben.orenstein@gmail.com>
2010-03-14 12:29:06 -0400

    Create table for Delayed::Job

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,22 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100302212814) do
+ActiveRecord::Schema.define(:version => 20100309011859) do
+
+  create_table "delayed_jobs", :force => true do |t|
+    t.integer  "priority",   :default => 0
+    t.integer  "attempts",   :default => 0
+    t.text     "handler"
+    t.string   "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["locked_by"], :name => "index_delayed_jobs_on_locked_by"
 
   create_table "uploads", :force => true do |t|
     t.string   "local_path"
